74d5e58be296568a3ebb157ebc9384a85a12bd7f
Neko Box Coder <nekoboxcoder@gmail.com>
2024-11-26 21:50:59 +0000

    Updating CMakeLists for new options and main example

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
-#if USE_SYSTEM2_SOURCE
+#if SYSTEM2_USE_SOURCE
     #define SYSTEM2_DECLARATION_ONLY 1
 #endif
 
+//This bypasses inheriting memory from parent process on linux (glibc 2.24) but removes the ability to use RunDirectory.
+//See https://github.com/Neko-Box-Coder/System2/issues/3
+//#define SYSTEM2_POSIX_SPAWN 1
+
+//#define SYSTEM2_DECLARATION_ONLY 1
+
+//#define SYSTEM2_IMPLEMENTATION_ONLY 1
+
 #include "System2.h"
 #include <stdio.h>
+
+#if SYSTEM2_TEST_MEMORY
+    #include <stdlib.h>
+#endif
 
 #define EXIT_IF_FAILED(result) \
 if(result != SYSTEM2_RESULT_SUCCESS) \
@@ -18,6 +30,11 @@ int main(int argc, char** argv)
     (void)argc;
     (void)argv;
     
+    #if SYSTEM2_TEST_MEMORY
+        void* testMem = malloc(50 * 1024 * 1024); //Test 50 MB
+        memset(testMem, 1, 50 * 1024 * 1024);
+    #endif
+    
     {
         System2CommandInfo commandInfo;
         memset(&commandInfo, 0, sizeof(System2CommandInfo));
@@ -26,11 +43,11 @@ int main(int argc, char** argv)
         SYSTEM2_RESULT result;
 
         #if defined(__unix__) || defined(__APPLE__)
-            result = System2Run("read testVar && echo testVar is \\\"$testVar\\\"", &commandInfo);
+            result = System2Run("sleep 5; read testVar && echo testVar is \\\"$testVar\\\"", &commandInfo);
         #endif
         
         #if defined(_WIN32)
-            result = System2Run("set /p testVar= && echo testVar is \"!testVar!\"", &commandInfo);
+            result = System2Run("timeout 5; set /p testVar= && echo testVar is \"!testVar!\"", &commandInfo);
         #endif
         
         EXIT_IF_FAILED(result);
@@ -51,6 +68,28 @@ int main(int argc, char** argv)
         EXIT_IF_FAILED(result);
         outputBuffer[bytesRead] = 0;
         
+        
+        //We can execute other commands while the previous one is still running
+        {
+            System2CommandInfo commandInfoInner;
+            memset(&commandInfoInner, 0, sizeof(System2CommandInfo));
+            SYSTEM2_RESULT resultInner;
+            #if defined(__unix__) || defined(__APPLE__)
+                resultInner = System2Run("sleep 2; echo Hello", &commandInfoInner);
+            #endif
+            
+            #if defined(_WIN32)
+                resultInner = System2Run("timeout 2; echo Hello", &commandInfoInner);
+            #endif
+            
+            EXIT_IF_FAILED(resultInner);
+            
+            int returnCode = -1;
+            resultInner = System2GetCommandReturnValueSync(&commandInfoInner, &returnCode);
+            EXIT_IF_FAILED(resultInner);
+        }
+        
+        
         int returnCode = -1;
         result = System2GetCommandReturnValueSync(&commandInfo, &returnCode);
         EXIT_IF_FAILED(result);
@@ -59,6 +98,7 @@ int main(int argc, char** argv)
         printf("%s: %d\n", "Command has executed with return value", returnCode);
     }
     
+    //Output: Hello
     //Output: Command has executed with return value: : 0
     //Output: testVar is "test content"
     
@@ -86,6 +126,10 @@ int main(int argc, char** argv)
         
         printf("%s: %d\n", "Command has executed with return value", returnCode);
     }
+    
+    #if SYSTEM2_TEST_MEMORY
+        free(testMem);
+    #endif
     
     return 0;
 }
