3fd3bbccdc2e42a60f631c4d98c876036d36c4a2
Neko Box Coder <nekoboxcoder@gmail.com>
2024-11-27 08:16:27 +0000

    Updating main example

--- main.c
+++ main.c
@@ -21,9 +21,78 @@
 if(result != SYSTEM2_RESULT_SUCCESS) \
 {\
     printf("Error at %d: %d", __LINE__, result);\
-    return -1;\
+    exit(-1);\
 }
 
+
+System2CommandInfo RedirectIOExample(void)
+{
+    System2CommandInfo commandInfo;
+    memset(&commandInfo, 0, sizeof(System2CommandInfo));
+    commandInfo.RedirectInput = true;
+    commandInfo.RedirectOutput = true;
+    SYSTEM2_RESULT result;
+
+    #if defined(__unix__) || defined(__APPLE__)
+        result = System2Run("sleep 1; read testVar && echo testVar is \\\"$testVar\\\"", &commandInfo);
+    #endif
+    
+    #if defined(_WIN32)
+        result = System2Run("timeout 1; set /p testVar= && echo testVar is \"!testVar!\"", &commandInfo);
+    #endif
+    
+    EXIT_IF_FAILED(result);
+    
+    char input[] = "test content\n";
+    result = System2WriteToInput(&commandInfo, input, sizeof(input));
+    EXIT_IF_FAILED(result);
+    
+    return commandInfo;
+}
+
+void BlockedCommandExample(void)
+{
+    System2CommandInfo commandInfo;
+    
+    memset(&commandInfo, 0, sizeof(System2CommandInfo));
+    SYSTEM2_RESULT result;
+    #if defined(__unix__) || defined(__APPLE__)
+        result = System2Run("sleep 2; echo Hello", &commandInfo);
+    #endif
+    
+    #if defined(_WIN32)
+        result = System2Run("timeout 2; echo Hello", &commandInfo);
+    #endif
+    
+    EXIT_IF_FAILED(result);
+    
+    int returnCode = -1;
+    result = System2GetCommandReturnValueSync(&commandInfo, &returnCode);
+    EXIT_IF_FAILED(result);
+}
+
+void StdinStdoutExample(void)
+{
+    System2CommandInfo commandInfo;
+    memset(&commandInfo, 0, sizeof(System2CommandInfo));
+    SYSTEM2_RESULT result;
+
+    #if defined(__unix__) || defined(__APPLE__)
+        result = System2Run("read testVar && echo testVar is \\\"$testVar\\\"", &commandInfo);
+    #endif
+    
+    #if defined(_WIN32)
+        result = System2Run("set /p testVar= && echo testVar is \"!testVar!\"", &commandInfo);
+    #endif
+    
+    EXIT_IF_FAILED(result);
+    
+    int returnCode = -1;
+    result = System2GetCommandReturnValueSync(&commandInfo, &returnCode);
+    EXIT_IF_FAILED(result);
+    
+    printf("%s: %d\n", "Command has executed with return value", returnCode);
+}
 
 int main(int argc, char** argv) 
 {
@@ -35,97 +104,53 @@ int main(int argc, char** argv)
         memset(testMem, 1, 50 * 1024 * 1024);
     #endif
     
-    {
-        System2CommandInfo commandInfo;
-        memset(&commandInfo, 0, sizeof(System2CommandInfo));
-        commandInfo.RedirectInput = true;
-        commandInfo.RedirectOutput = true;
-        SYSTEM2_RESULT result;
+    //Execute the first command
+    System2CommandInfo commandInfo = RedirectIOExample();
 
-        #if defined(__unix__) || defined(__APPLE__)
-            result = System2Run("sleep 5; read testVar && echo testVar is \\\"$testVar\\\"", &commandInfo);
-        #endif
-        
-        #if defined(_WIN32)
-            result = System2Run("timeout 5; set /p testVar= && echo testVar is \"!testVar!\"", &commandInfo);
-        #endif
-        
-        EXIT_IF_FAILED(result);
-        
-        char input[] = "test content\n";
-        result = System2WriteToInput(&commandInfo, input, sizeof(input));
-        EXIT_IF_FAILED(result);
-        
-        //Waiting here simulates the child process has "finished" and we read the output of it
-        //Sleep(2000);
-        
+    //We can execute other commands while the previous one is still running
+    //Output: Hello
+    BlockedCommandExample();
+    
+    //The first command should be finish by now. We need to clear the redirected output first
+    //  otherwise the process won't terminate.
+    {
         char outputBuffer[1024];
-        uint32_t bytesRead = 0;
         
         //System2ReadFromOutput can also return SYSTEM2_RESULT_READ_NOT_FINISHED if we have more to read
-        //In which case can use a do while loop to keep getting the output
-        result = System2ReadFromOutput(&commandInfo, outputBuffer, 1023, &bytesRead);
-        EXIT_IF_FAILED(result);
-        outputBuffer[bytesRead] = 0;
-        
-        
-        //We can execute other commands while the previous one is still running
+        //Output: testVar is "test content"
+        SYSTEM2_RESULT result;
+        do
         {
-            System2CommandInfo commandInfoInner;
-            memset(&commandInfoInner, 0, sizeof(System2CommandInfo));
-            SYSTEM2_RESULT resultInner;
-            #if defined(__unix__) || defined(__APPLE__)
-                resultInner = System2Run("sleep 2; echo Hello", &commandInfoInner);
-            #endif
-            
-            #if defined(_WIN32)
-                resultInner = System2Run("timeout 2; echo Hello", &commandInfoInner);
-            #endif
-            
-            EXIT_IF_FAILED(resultInner);
-            
-            int returnCode = -1;
-            resultInner = System2GetCommandReturnValueSync(&commandInfoInner, &returnCode);
-            EXIT_IF_FAILED(resultInner);
+            uint32_t bytesRead = 0;
+            result = System2ReadFromOutput(&commandInfo, outputBuffer, 1023, &bytesRead);
+            outputBuffer[bytesRead] = 0;
+            printf("%s", outputBuffer);
         }
+        while(result == SYSTEM2_RESULT_READ_NOT_FINISHED);
         
-        
-        int returnCode = -1;
-        result = System2GetCommandReturnValueSync(&commandInfo, &returnCode);
         EXIT_IF_FAILED(result);
-        
-        printf("%s", outputBuffer);
-        printf("%s: %d\n", "Command has executed with return value", returnCode);
     }
     
-    //Output: Hello
-    //Output: Command has executed with return value: : 0
-    //Output: testVar is "test content"
+    //Now we can safely check the output of a process without blocking
+    //Output: 1st command has finished with return value: : 0
+    {
+        int returnCode = -1;
+        SYSTEM2_RESULT result = System2GetCommandReturnValueAsync(&commandInfo, &returnCode);
+        
+        if(result == SYSTEM2_RESULT_COMMAND_NOT_FINISHED)
+            printf("1st command not yet finished");
+        else
+        {
+            EXIT_IF_FAILED(result);
+            printf("%s: %d\n", "1st command has finished with return value", returnCode);
+        }
+    }
     
     printf("\nUsing stdin now, enter the value of testVar: ");
     fflush(stdout);
     
-    {
-        System2CommandInfo commandInfo;
-        memset(&commandInfo, 0, sizeof(System2CommandInfo));
-        SYSTEM2_RESULT result;
-
-        #if defined(__unix__) || defined(__APPLE__)
-            result = System2Run("read testVar && echo testVar is \\\"$testVar\\\"", &commandInfo);
-        #endif
-        
-        #if defined(_WIN32)
-            result = System2Run("set /p testVar= && echo testVar is \"!testVar!\"", &commandInfo);
-        #endif
-        
-        EXIT_IF_FAILED(result);
-        
-        int returnCode = -1;
-        result = System2GetCommandReturnValueSync(&commandInfo, &returnCode);
-        EXIT_IF_FAILED(result);
-        
-        printf("%s: %d\n", "Command has executed with return value", returnCode);
-    }
+    //If you don't do redirect, it will directly use stdin and stdout instead
+    StdinStdoutExample();
     
     #if SYSTEM2_TEST_MEMORY
         free(testMem);
